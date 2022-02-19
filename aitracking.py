9ac857672eb50505de2f5d038d9096eed93a43c8
yagyan <60309135+yagyan@users.noreply.github.com>
2022-02-19 12:34:38 +0545

    added gui and right click functionality

--- aitracking.py
+++ aitracking.py
@@ -1,5 +1,9 @@
+import sys
+
 import cv2
 import numpy as np
+import pyautogui
+
 import HandTrackingModule as htm
 import autopy
 from ctypes import cast, POINTER
@@ -9,6 +13,7 @@ import screen_brightness_control as sbc
 import findDistance as FD
 import time
 from pynput.keyboard import Key, Controller
+import gui
 
 keyboard = Controller()
 ##########################
@@ -41,15 +46,30 @@ volBar = 400
 volPer = 0
 area = 0
 colorVol = (255, 0, 0)
-dominant = "Right"
+dominant = gui.dominant
+left = gui.left
+right = gui.right
+Evol = gui.volume
+Ebri = gui.brightness
+cancel = gui.cancel
+movement= gui.movement
+value = 1
 flag = "00"
+f= 0
+if dominant == 'Right':
+    value = 1
+if dominant == 'Left':
+    value = 0
 
+if cancel:
+    sys.exit()
 while True:
     # 1. Find hand Landmarks
     success, img = cap.read()
     img = cv2.flip(img, 1)
     img = detector.findHands(img, draw=True)
     lmList, bbox, label = detector.findPosition(img)
+
     # 2. Get the tip of the index and middle fingers
 
     print(label)
@@ -57,7 +77,11 @@ while True:
         x1, y1 = lmList[8][1:3]
         x2, y2 = lmList[12][1:3]
         # print(x1, y1, x2, y2)
+        if flag == "00" or flag == "01":
+            cv2.putText(img, "int= Off", (450, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
+        if flag == "11":
+            cv2.putText(img, "int= On", (450, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
         # 3. Check which fingers are up
         fingers = detector.fingersUp()
         # print(fingers)
@@ -65,16 +89,17 @@ while True:
 
         if flag == "00" and label == dominant and fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[
             4] == 0 and \
-                fingers[0] == 1:
+                fingers[0] == value:
             flag = "01"
+
         if flag == "01" and label == dominant and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and fingers[
-            4] == 1 and fingers[0] == 0:
+            4] == 1 and fingers[0] != value:
             flag = "11"
         print(flag)
         if flag == "11":
             # 4. Only Index Finger : Moving Mode
-            if label == dominant and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0 and \
-                    fingers[0] == 1:
+            if movement== 'Enable' and label == dominant and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0 and \
+                    fingers[0] == value:
                 # 5. Convert Coordinates
                 x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
                 y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
@@ -83,26 +108,31 @@ while True:
                 clocY = plocY + (y3 - plocY) / smoothening
 
                 # 7. Move Mouse
+                f = 0
                 autopy.mouse.move(clocX, clocY)
                 cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
                 plocX, plocY = clocX, clocY
 
-
-
-            # 8. Both Index and middle fingers are up : Clicking Mode
-            if label == dominant and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0 and \
-                    fingers[0] == 1:
+            # 8. Both Index and middle fingers are up : left Clicking Mode
+            if left == "Enable" and label == dominant and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0 and \
+                    fingers[0] == value:
                 # 9. Find distance between fingers
+
                 length, img, lineInfo = FD.findDistance(lmList, 8, 12, img)
                 # print(length)
                 # 10. Click mouse if distance short
                 if length < 40:
                     cv2.circle(img, (lineInfo[4], lineInfo[5]),
                                15, (0, 255, 0), cv2.FILLED)
-                    autopy.mouse.click()
-                    #pyautogui.click(button='left')
+                    pyautogui.click(button='left')
 
-            if label != dominant and fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and \
+            #         right click
+            if right == 'Enable' and label == dominant and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0 and \
+                    fingers[0] != value and f == 0:
+                pyautogui.click(button='right')
+                f = 1
+
+            if Evol == "Enable" and label != dominant and fingers[0] == value and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and \
                     fingers[4] == 0:
                 # Filter based on size
                 area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
@@ -122,32 +152,34 @@ while True:
                     volume.SetMasterVolumeLevelScalar(volPer / 100, None)
                     cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                     colorVol = (0, 255, 0)
+                    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+                    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
-            if label != dominant and fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and \
+            if Ebri == "Enable" and label != dominant and fingers[0] == value and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and \
                     fingers[4] == 0:
                 # Filter based on size
                 area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1]) // 100
                 # print(area)
                 if 250 < area < 1000:
-                    length1, img, lineInfo = FD.findDistance(lmList, 4, 12, img)
+                    length1, img, lineInfo = FD.findDistance(lmList, 4, 8, img)
                     bBar = np.interp(length1, [50, 200], [400, 150])
                     bPer = np.interp(length1, [50, 200], [0, 100])
                     # Reduce Resolution to make it smoother
                     smoothness = 10
                     bPer = smoothness * round(bPer / smoothness)
                     sbc.set_brightness(bPer)
+                    cv2.rectangle(img, (50, int(bBar)), (85, 400), (255, 0, 0), cv2.FILLED)
+                    cv2.putText(img, f'{int(bPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
-            # if label == dominant and fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and \
-            #         fingers[4] == 1:
-            #     keyboard.press(Key.ctrl)
-            #     keyboard.press('a')
-            #     keyboard.release(Key.ctrl)
-            #     keyboard.release('a')
+            if label == dominant and fingers[0] == value and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1 and \
+                    fingers[4] == 1:
+                keyboard.press(Key.ctrl)
+                keyboard.press('a')
+                keyboard.release(Key.ctrl)
+                keyboard.release('a')
 
     # cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
-    # cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    # cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-    #             1, (255, 0, 0), 3)
+
     # cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
     # cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
     #             1, colorVol, 3)
